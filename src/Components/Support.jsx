07169d6ac7a6c6873147988804e523a879978b55
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2024-10-07 20:50:58 +0000

    responsive text

--- src/Components/Support.jsx
+++ src/Components/Support.jsx
@@ -1,10 +1,12 @@
 function Support() {
-    return(
+    return (
         <div className="welcomediv position-relative">
             <div className="margen">
                 <div className="">
-                    <h1 className="d-flex justify-content-center">Support trufaeu</h1>
-                    <p className="d-flex justify-content-center p-3 App">Every share, like, comment, and purchase is a powerful way to<br /> support your favorite artist. By engaging with an artist's work<br /> on social media, buying their merchandise, or donating to their<br /> projects, you're directly contributing to their success.<br /><br /> Show your support to uplift the creative community!</p>
+                    <h1 className="d-flex justify-content-center p-2">Support trufaeu</h1>
+                    <div className="container3">
+                    <p className="textoresponsive d-flex justify-content-center p-2 App">Every share, like, comment, and purchase is a powerful way to support your favorite artist. By engaging with an artist's work on social media, buying their merchandise, or donating to their projects, you're directly contributing to their success.<br /><br /> Show your support to uplift the creative community!</p>
+                    </div>                    
                     <img className="position-absolute bottom-0 end-0 translate-middle imgverde" src="Qr.png" alt="" />
                 </div>
             </div>
