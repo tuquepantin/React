78536ce34166b6741f47369f75794e50b1227483
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2024-10-07 00:58:39 +0000

    mas cambios pequeñs

--- src/Components/Myinfo.jsx
+++ src/Components/Myinfo.jsx
@@ -6,7 +6,7 @@ function Myinfo() {
                     <div class="col-md-7">
                         <div>
                             <h2>Meet the artist <br /> behind trufaeu</h2>
-                            <div style={{ width: '70%', height: '25vh' }}>
+                            <div >
                                 <p>Eugenia Pantin (trufaeu) is a talented illustrator from Venezuela, who creates captivating artwork that brings her imagination to life. With her impressive style and attention to detail, she is able to capture the essence of any subject and create stunning visuals that resonate with viewers. Whether you're looking for a custom illustration, a print to decorate your space, or a piece of merchandise that showcases trufaeu's art, you'll find it all here. Explore her portfolio and be inspired by her creativity!</p>
                                 <p>Email: contact@trufaeu.com</p>
                             </div>
@@ -19,7 +19,7 @@ function Myinfo() {
 
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row p-3">
                         <div class="col-md-2">
                         <p className="App" style={{backgroundColor: '#D9D9D9', width: '95px'  }}>Portfolio</p>
                         </div>
